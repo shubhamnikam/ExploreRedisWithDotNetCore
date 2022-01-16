ebc0a6485a7b5207fd466b26855f40ffbb93b986
Shubham Nikam <shubhamsnikam1@gmail.com>
2022-01-15 21:03:33 -0800

    Memwory Cache and Redis Cache done

---
+++ API.Redis.Cache/Services/RedisCacheService.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace API.Redis.Cache.Services
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IConnectionMultiplexer connectionMultiplexer;
+
+        public RedisCacheService(IConnectionMultiplexer connectionMultiplexer)
+        {
+            this.connectionMultiplexer = connectionMultiplexer;
+        }
+        public async Task<string> GetCacheValueAsync(string key)
+        {
+            var db = connectionMultiplexer.GetDatabase();
+            return await db.StringGetAsync(key);
+        }
+
+        public Task SetCacheValueAsync(string key, string value)
+        {
+            var db = connectionMultiplexer.GetDatabase();
+            db.StringSetAsync(key, value);
+            return Task.CompletedTask;
+        }
+    }
+}
